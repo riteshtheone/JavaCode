15d121a40262f9dac9222969b89dc77b3e11d29c
Ritesh Kumar <riteshnagwan@gmail.com>
2020-11-05 22:26:02 +0530

    no massage

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,20 @@
+package sorting;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static int[] bubbleSort(){
+	
+	return new array[0];
+	}
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length of an arrray:- ");
+		int n = sc.nextInt();
+		int[] ar = new int[n];
+
+       	}
+}
