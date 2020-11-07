96a9fc080edc203be7e61b1319acf2215e28ff97
Ritesh Kumar <riteshnagwan@gmail.com>
2020-11-07 22:16:31 +0530

    Added new Sorting Algorithm

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,20 +1,52 @@
 package sorting;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class BubbleSort {
 
-	public static int[] bubbleSort(){
-	
-	return new array[0];
+	public static int[] bubbleSort(int[] ar){
+		for(int i=0; i<ar.length-1; i++){
+			int temp;
+			boolean isSorted = true;
+			for(int j=0; j<ar.length-(i+1); j++){
+				if(ar[j]>ar[j+1]){
+					temp = ar[j];
+					ar[j] = ar[j+1];
+					ar[j+1] = temp;
+					isSorted = false;
+				}
+			}
+			if (isSorted) break;
+		}			
+         return ar;
 	}
 
 	public static void main(String[] args){
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the length of an arrray:- ");
+		System.out.print("Enter the length of an array:- ");
 		int n = sc.nextInt();
 		int[] ar = new int[n];
 
-       	}
+		if (n>0)
+			System.out.println("Enter the elements in array:- ");
+			for(int i=0; i<n; i++){
+				System.out.print((i+1)+" -> ");
+		 	  	ar[i] = sc.nextInt();
+			}
+
+		ar = bubbleSort(ar);
+		System.out.print("Sorted array is:- ");
+
+		for(int i=0; i<n; i++){
+			if (i==0) System.out.print("[");
+			if(i==n-1){
+				System.out.print(ar[i]);
+			}else{
+				System.out.print(ar[i]+", ");
+			}
+			if (i==n-1) System.out.print("]");
+		}
+		sc.close();
+	}
 }
---
+++ src/sorting/SelectionSort.java
@@ -0,0 +1,52 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static int[] selectionSort(int[] ar){
+
+        int min,temp;
+        for (int i=0 ; i<ar.length-1 ; i++){
+            min = i;
+            for (int j=i+1 ; j<ar.length ; j++){
+                if (ar[j]<ar[min]){
+                    min = j;
+                }
+            }
+            temp = ar[i];
+            ar[i] = ar[min];
+            ar[min] = temp;
+        }
+        return ar;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of an array:- ");
+        int n = sc.nextInt();
+        int[] ar = new int[n];
+
+        if (n>0)
+            System.out.println("Enter the elements in array:- ");
+        for(int i=0; i<n; i++){
+            System.out.print((i+1)+" -> ");
+            ar[i] = sc.nextInt();
+        }
+
+        ar = selectionSort(ar);
+        System.out.print("Sorted array is:- ");
+
+        for(int i=0; i<n; i++){
+            if (i==0) System.out.print("[");
+            if(i==n-1){
+                System.out.print(ar[i]);
+            }else{
+                System.out.print(ar[i]+", ");
+            }
+            if (i==n-1) System.out.print("]");
+        }
+        sc.close();
+    }
+}
