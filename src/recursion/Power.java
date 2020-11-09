d7d243a5204f2d675461007256b8f5163ebfb38c
Ritesh Kumar <riteshnagwan@gmail.com>
2020-11-09 20:52:38 +0530

    Modified

--- src/recursion/Power.java
+++ src/recursion/Power.java
@@ -4,10 +4,7 @@ import java.util.Scanner;
 
 public class Power {
 
-    static int step = 0;
-
     public static int power(int n , int p) {
-        step++;
         if (p<=0) return 1;
         return p%2==0 ? power(n*n,p/2) : n*power(n*n,(p-1)/2) ;
     }
@@ -19,7 +16,6 @@ public class Power {
         System.out.print("Enter the Power:- ");
         int p = sc.nextInt();
         System.out.println(power(n,p));
-        System.out.println(step);
         sc.close();
 
         }
