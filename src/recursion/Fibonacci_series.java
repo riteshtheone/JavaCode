749834507bebc6d8a2c7303b4ef4826ddbe64e93
Ritesh Kumar <riteshnagwan@gmail.com>
2020-11-18 23:47:51 +0530

    new code Find Fibonacci_Series

---
+++ src/recursion/Fibonacci_series.java
@@ -0,0 +1,32 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class Fibonacci_series {
+
+    public static int a=0,b=1,c;
+    public static boolean first=true;
+    
+    public static void fibonacci_series(int n){
+        if (first){
+            if (n==1){
+                System.out.print(a);
+            }else if (n>1){
+                 System.out.print(a+" "+b+" ");
+                 first=false;
+                 fibonacci_series(n-1);
+            }
+        }else if (n>1){
+            System.out.print((c=a+b)+ " ");
+            a=b;
+            b=c;
+            fibonacci_series(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number :- ");
+        fibonacci_series(sc.nextInt());
+    }
+}
